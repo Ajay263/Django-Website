26e74cf1f1d25e80eb9f6dc60d650176721458d5
Ajay263 <97569992+Ajay263@users.noreply.github.com>
2022-04-21 14:57:40 +0200

    Update settings.py
    removed Heroku settings

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 
 
@@ -134,7 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
